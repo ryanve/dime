b5175abffd5fb405b2024980e13b073f433ee164
Ryan Van Etten <ryanve@gmail.com>
2012-10-26 15:08:59 -0400

    Version 2.0.0 simplies dime into a much more compact purposeful module for dimensions.

--- README.md
+++ README.md
@@ -1,77 +1,41 @@
-[dime](https://github.com/ryanve/dime) is an ultra-fast open-source cross-browser micro-library for calculating dimensions, using pure native JavaScript methods.
+[dime](https://github.com/ryanve/dime) is cross-browser module for calculating dimensions in JavaScript.
 
-## API
+```
+$ npm install dime
+```
 
-### Methods
+# methods
+
+### static
+
+Cheap simple methods are on the top-level:
 
 ```javascript
-dime.deviceW()   // get device width
+dime.device()    // get object containing width and height
+dime.deviceW()   // get device width 
 dime.deviceH()   // get device height
-dime.deviceMax() // get Math.max(dime.deviceW, dime.deviceH)
-dime.deviceMin() // get Math.min(dime.deviceW, dime.deviceH)
+dime.viewport()  // get object containing current width and height
 dime.viewportW() // get viewport width
 dime.viewportH() // get viewport height
-dime.documentW() // get document width
-dime.documentH() // get document height
-dime.overflowX() // get # of horizontal pixels that doc overflows viewport (or 0 if no overflow)
-dime.overflowY() // get # of vertical pixels that doc overflows viewport (or 0 if no overflow)
-dime.scrollX()   // cross-broswer equiv to native window.scrollX
-dime.scrollY()   // cross-broswer equiv to native window.scrollY
-
-dime.is(method, min [, max]) // boolean for testing ranges
-dime.is('viewportW', 800) // equiv to (dime.viewportW() >= 800)
-dime.is('deviceH', 480, 960) // equiv to (dime.deviceH() >= 480 && dime.deviceH() <= 960)
-// ... works for any of the methods listed above
-
-dime(elem) // wrapper for element-based methods (accepts native DOM elements, document, or window)
-dime(elem).rectangle() // cross-browser getBoundingClientRect: .top/.bottom/.left/.right/.width/.height
-dime(elem).offset() // coordinates relative to the document: .top/.bottom/.left/.right/.width/.height
-dime(elem).get(index) // applicable when dime is paired w/ a query engine (index -1 gets the last elem)
-dime(elem).element() // if elem (index 0) is a DOM element, get it. Otherwise get the documentElement.
-
 ```
 
-### Extending
+### chain 
 
-Devs can add a query engine such as [qwery](https://github.com/ded/qwery) to provide the ability to query selector strings like `dime('#example')`
+jQuery-compatible-ish methods designed to be mixed into a jQuery-like lib:
 
 ```javascript
-
-dime.setQueryEngine()       // set query engine to $
-dime.setQueryEngine(qwery)  // set query engine to qwery
-dime.setQueryEngine(jQuery) // set query engine to jQuery
-
+dime.fn.width() // get the width of the 1st elem in the set
+dime.fn.width(value) // set the width of all elems in the set
+dime.fn.height() // get the width of the 1st elem in the set
+dime.fn.height(value) // set the width of all elems in the set
 ```
 
-### Instances
+## related
 
-If you need to check whether an unknown object is an instance of `dime()` you can use either of these techniques:
+- [Response](https://github.com/ryanve/response.js)
+- [verge](https://github.com/ryanve/verge)
 
-```javascript
-
-ukn instanceof dime // true if ukn is an instance of dime()
-ukn && ukn.dime     // true if ukn is an instance of dime()
-
-```
-The result of calling the dime() function is an array-like object. It has a length property representing the number of elems. If you haven't added a query engine, then the length will be `1` and that element will be stored in index 0, unless you pass it something invalid then the length property will be `0` and index 0 will be `false`. If you've added an engine, the length will be the number of selected elements.
-
-```javascript
-var elem, dimeElem;
-elem = document.getElementById("example");
-dimeElem = dime(elem);
-!!dimeElem.length;      // true if #example exists
-1 === dimeElem.length;  // true if #example exists
-elem === dimeElem[0];   // true if #example exists (otherwise false)
-true === dimeElem.dime; // true if it exists *or* if it doesn't
-
-```
-
-### Projects
-
-Much of [dime](https://github.com/ryanve/dime) is also built into [Response](https://github.com/ryanve/response.js) 0.3+
-
-
-## License
+# license
 
 ### [dime](https://github.com/ryanve/dime) is available under the [MIT license](http://en.wikipedia.org/wiki/MIT_License)
 
--- dime.js
+++ dime.js
@@ -1,260 +1,238 @@
 /*!
- * dime       ultra-fast cross-browser micro-lib for calculating dimensions
+ * dime       cross-browser module for calculating dimensions
  * @author    Ryan Van Etten (c) 2012
  * @link      http://github.com/ryanve/dime
  * @license   MIT
- * @version   1.1.0
+ * @version   2.0.0
  */
 
-!(function (name, definition) {
-    if (typeof module !== 'undefined') {
-        // Node (nodejs.org)
-        module.exports = definition();
-    }
-    else if (typeof define === 'function' && define.amd) {
-        // AMD (github.com/amdjs/amdjs-api/wiki/AMD)
-        // addyosmani.com/writing-modular-js/
-        define(name, definition);
-    }
-    else {
-        // Standalone:
-        // this === window in the global scope
-        this[name] = definition();
-    }
-}('dime', function() {// Immediately invoke the above func, passing 'dime' to the
-                      // name param, and passing the entire callback below to the
-                      // definition param. (Props to github.com/ded for this setup)
+/*jslint browser: true, devel: true, node: true, passfail: false, bitwise: true, continue: true
+, debug: true, eqeq: true, es5: true, forin: true, newcap: true, nomen: true, plusplus: true
+, regexp: true, undef: true, sloppy: true, stupid: true, sub: true, vars: true, white: true
+, indent: 4, maxerr: 180 */
+ 
+(function (root, name, definition) {// github.com/umdjs/umd
+    if ( typeof module != 'undefined' && module.exports ) { 
+        module.exports = definition(); // node / common / ender
+    } else { root[name] = definition(); } // browser
+}(this, 'dime', function () {
 
-    var prop
-      , win = window
-      , screen = win.screen
-      , doc = win.document
-      , max = win.Math.max
-      , html = doc.documentElement
+    var win = window
+      , doc = document
+      , docElem = doc.documentElement
       , screenW = screen.width
       , screenH = screen.height
-      , screenMax = max(screenW, screenH)
+      , screenMax = screenW > screenH ? screenW : screenH
       , screenMin = screenW + screenH - screenMax
-      , queryEngine = false
-    ;
+      , capName = { 'width': 'Width', 'height': 'Height' }
+      , effin = {};
     
-    function dime(el) {
-        
-        // This function is setup in a way that makes
-        // it so that  dime() instanceof dime === true
-        // similiar to the jQuery function. See 31:00
-        // in @link youtube.com/watch?v=0LKDImgRfrg
-        
-        // The next two lines are something like Object.create
-        // See @link javascript.crockford.com/prototypal.html
-        
-        function object() {}
-        object.prototype = dime.prototype;
-
-        var instance = new object() // create instance
-          , nodeType
-          , isElemDocOrWin;
-        
-        // If an engine has been set and el is a string then query it:
-        if (queryEngine && typeof 'el' === 'string') {
-            instance.selector = el;
-            el = queryEngine(el)[0];
-            instance.length = el.length || 0;
-        }
-
-        // Check if el is a DOM element, document, or window:
-        // developer.mozilla.org/en/nodeType
-        nodeType = el ? el.nodeType : el;
-        isElemDocOrWin = !!(nodeType === 1 || nodeType === 9 || el === win);
-
-        // Put the element/doc/win (or false) in index 0:
-        instance[0] = isElemDocOrWin ? el : false;
-        
-        // Make it work as an array-like object. Also this make it so we can 
-        // check !!this.length to verify that the object has something in it:
-        if (instance.selector) {
-            instance.length = isElemDocOrWin ? 1 : 0;
-        }
-        
-        return instance;
-    }
-    
-    dime.prototype = {
-        
-        // There's lots of info about how these calculations
-        // are computed @link responsejs.com/labs/dimensions/
-
-        dime: true // Make it easy to check for dime instances.
-
-      , length: 0  // Gets overwritten to 1 when dime(somethingReal) is called.
-
-      , selector: null // Becomes a string only if a query engine is used.
-      
-        /**
-         * dime.setQueryEngine()    Integrate a query engine into dime so that you 
-         *                          can pass selector strings like dime('#example')
-         *
-         * @example   dime.setQueryEngine(qwery);        // dustindiaz.com/qwery
-         * @example   dime.setQueryEngine(jQuery);       // jquery.com
-         *
-         */
-
-      , setQueryEngine: function(e) {
-              // Inspired by bonzo.setQueryEngine @link github.com/ded/bonzo
-            if (!arguments.length && !queryEngine) {
-                queryEngine = win.$;
-            }
-            else if (typeof e === 'function') {
-                queryEngine = e;
-            }
-            return !!queryEngine;
-        }
-
-      , element: function() {
-            // Get the element in index 0 or the docElem if index 0 is not an element. This is used
-            // in methods that require an element (to make sure we're always dealing w/ an element).
-            var el = this[0];
-            return 1 === el.nodeType ? el : html;
-        }
-
-      , get: function(index) {
-            index = index || 0;
-            if ( index < 0 ) {
-                index = this.length + index;
-            }
-            return dime(this[index]);
-        }
-
-      , is: function(method, min, max) {
-            var type = typeof method
-              , num = 'string' === type ? dime[method]() : 'function' === type ? method() : false;
-            return 'number' === typeof num ? (num >= (min || 0) && (!max || num <= max)) : false;
-        }
-
-        // responsejs.com/labs/dimensions/#device
-      , deviceW:   function() { 
-            return screenW; 
-        }
-      , deviceH:   function() {
-            return screenH; 
-        }
-      , deviceMax: function() { 
-            return screenMax; 
-        }
-      , deviceMin: function() { 
-            return screenMin; 
-        }
-          
-        // responsejs.com/labs/dimensions/#viewport
-      , viewportW: function() { 
-            return html.clientWidth; 
-        }
-      , viewportH: function() { 
-            return html.clientHeight; 
-        }
-
-        // responsejs.com/labs/dimensions/#document
-      , documentW: function() {
-            return max(html.offsetWidth, html.scrollWidth, doc.body.scrollWidth);
-        }
-      , documentH: function() {
-            return max(html.offsetHeight, html.scrollHeight, doc.body.scrollHeight);
-        }
-
-      , scrollX: function() {
-            // developer.mozilla.org/en/DOM/window.scrollX
-            return win.pageXOffset || html.scrollLeft; 
-        }
-        
-      , scrollY: function() {
-            // developer.mozilla.org/en/DOM/window.scrollY
-            return win.pageYOffset || html.scrollTop; 
-        }
-        
-      , overflowX: function() {
-            var difference = dime.documentW() - dime.viewportW();
-            return 0 < difference ? difference : 0;
-        }
-            
-      , overflowY: function() {
-            var difference = dime.documentH() - dime.viewportH();
-            return 0 < difference ? difference : 0;
-        }
-
-        /**
-         * .rectangle()    Cross-browser safe version of getBoundingClientRect. The
-         *                 top/bottom/left/right props are pretty widlely supported already.
-         *                 The width/height props are patched for browsers that don't natively 
-         *                 support them. Firefox returns coords as floats while other browsers
-         *                 return them as integers. 
-         *
-         * @link developer.mozilla.org/en/DOM/element.getBoundingClientRect
-         * @link quirksmode.org/dom/w3c_cssom.html
-         * @return  object
-         */
-        
-      , rectangle: function () {
-            var el = this.element()
-              , method = 'getBoundingClientRect'
-              , r = el[method] ? el[method]() : {}
-            ;
-            // The native object is read-only so we 
-            // have use a copy in order to modify it:
-            return {
-                top: r.top || 0
-              , left: r.left || 0
-              , bottom: r.bottom || 0
-              , right: r.right || 0
-              , width: r.width || (r.right - r.left)
-              , height: r.height || (r.bottom - r.top)
-            };
-        }
-          
-        /**
-         * .offset()       Get the coordinates of this.element() relative to the document.
-         *                 This returns an object like that of api.jquery.com/offset/
-         */
-          
-      , offset: function() {
-              // Adapted from jQuery .offset()
-            var r = this.rectangle()
-              , x = dime.scrollX() - (html.clientLeft || 0)
-              , y = dime.scrollY() - (html.clientTop || 0)
-            ;
-            return {
-                top: r.top + y
-              , left: r.left + x
-              , bottom: r.bottom + y
-              , right: r.right + x
-              , width: r.width
-              , height: r.height
-            };
-        }
-      
-    };//dime.prototype
-    
-    dime.prototype[0] = false;
-    
-    // So for instances of dime() include all the props/methods in
-    // dime.prototype  The loop below also adds them directly onto
-    // dime so that static props can be called like dime.viewportW()
-    // rather than dime().viewportW() - The methods are defined so 
-    // both ways both will work, but the static form is faster b/c
-    // it requires one less function call.
-    
-    for (prop in dime.prototype) {
-        if (dime.prototype.hasOwnProperty(prop)) {
-            dime[prop] = dime.prototype[prop];
+   /**
+    * @constructor  a very lo-fi one (it only grabs the 1st elem)
+    * @param  {*=}  item
+    */
+    function Api (item) {
+        this.length = 0;
+        if ( item = item.nodeType > 0 || item === win ? item : item && item[0] ) {
+            this[ this.length++ ] = item;
         }
     }
     
-    return dime;
+    /**
+     * @param  {*=}  item
+     */
+    function api (item) {
+        return new Api(item);
+    }
 
-})); // End callback and closure.
+    // read @link github.com/ender-js/ender-js/pull/17
+    api.prototype = api['fn'] = Api.prototype = effin;
+    //var api = {'fn': effin};
+    
+    // responsejs.com/labs/dimensions/#device
+    function deviceW () { 
+        return screenW; 
+    }
+    function deviceH () {
+        return screenH; 
+    }
+    function deviceMax () { 
+        return screenMax; 
+    }
+    function deviceMin () { 
+        return screenMin; 
+    }
+    
+    // responsejs.com/labs/dimensions/#viewport
+    function viewportW () {
+        return docElem.clientWidth; 
+    }
+    function viewportH () {
+        return docElem.clientHeight; 
+    }
 
-// Recommended videos to watch related
-// to the design patterns used in dime:
-// youtube.com/watch?v=i_qE1iAmjFg // Paul Irish
-// youtube.com/watch?v=0LKDImgRfrg // John Resig
+    /**
+     * @param  {string}  name
+     * @return {number}
+     */    
+    function vpGetter ( name ) {
+        return docElem[ 'client' + capName[name] ];
+    }
+    
+    /**
+     * @return {Object}
+     */
+    function viewport ( name ) {
+        return null == name ? {
+            'width': docElem.clientWidth
+          , 'height': docElem.clientHeight
+        } : vpGetter( name );  
+    }
 
-/*jslint browser: true, es5: true, white: true, newcap: true, plusplus: true, regexp: true, maxerr: 50, indent: 4 */
+    /**
+     * @return {Object}
+     */
+    function device ( name ) {
+        var ob = { 'width': screenW, 'height': screenH };
+        return null == name ? ob : ob[name];
+    }
+	
+	/**
+     * @param  {string}  name
+     * @return {number}
+     */
+    function docGetter ( name ) {
+        var scroll, offset;
+        name = capName[name];
+        scroll = 'scroll' + name;
+        offset = 'offset' + name;
+        return Math.max( docElem[offset], docElem[scroll], doc.body[scroll] );
+    }
+
+    // responsejs.com/labs/dimensions/#document
+    function documentW () {
+        return docGetter('width');
+    }
+    function documentH () {
+        return docGetter('height');
+    }
+    
+    /**
+     * read an element's dimensions (even if it is hidden)
+     * @param  {Object}    el
+     * @param  {Function}  fn
+     * @param  {Object=}   scope
+     */
+    function peek ( el, fn, scope ) {
+    
+        if ( !el ) { return; }
+        var oPos, oVis, oDis, ret
+          , tPos, tVis, tDis
+          , w = el.offsetWidth
+          , h = el.offsetHeight
+          , isHidden = !w && !h;
+
+        if ( isHidden ) {// save orig styles and set temp styles:
+            oPos = el.style.position || '';
+            oVis = el.style.visibility || '';
+            oDis = el.style.display || '';
+            el.style.position = tPos = 'absolute';
+            el.style.visibility = tVis = 'hidden';
+            el.style.display = tDis = 'block';
+        }
+
+        ret = fn.call( scope || el, el );
+        
+        if ( isHidden ) {// put back the orig values, unless `fn` reset them:
+            tPos === el.style.position && (el.style.position = oPos);
+            tVis === el.style.visibility && (el.style.visibility = oVis);
+            tDis === el.style.display && (el.style.display = oDis);
+        }
+        
+        return ret;
+
+    }// peek
+    
+    /**
+     * @param  {Array|Object}  els
+     * @param  {string}        name
+     * @param  {string}        value
+     */
+    // inlined @ minification
+    function setStyle ( els, name, value ) {
+        var i = 0, l = els.length;
+        while ( i < l ) {
+            els[i++]['style'][name] = value;
+        }
+        return els;
+    }
+    
+    /**
+     * Handler for the .width/.height methods
+     * @param  {Array|Object}  els
+     * @param  {string}        name
+     * @param  {string|number} px
+     */
+    function setPx ( els, name, px ) {
+        px = typeof px == 'number' ? '' + px + 'px' : px;
+        return setStyle( els, name, px );
+    }
+
+    /**
+     * Handler for the .width/.height methods
+     * @param  {string}            name
+     * @param  {Array|Object}      els
+     * @return {number|undefined}
+     */
+	function getPx ( els, name ) {
+		if ( els[0] ) {
+            if ( 1 === els[0].nodeType ) {
+                return peek( els[0], function (e) { 
+                    return e[ 'offset' + capName[name] ]; 
+                });
+            }
+            if ( els[0] === win ) { 
+                return vpGetter(name); 
+            }
+            if ( els[0] === doc ) { 
+                return docGetter(name); 
+            }
+        }
+    }
+    
+    /**
+     * @param  {(number|string)=}  px
+     */
+    function width ( px ) {
+        return ( void 0 === px ? getPx : setPx )( this, 'width', px  );
+    }
+    
+    /**
+     * @param  {(number|string)=}  px
+     */
+    function height ( px ) {
+        return ( void 0 === px ? getPx : setPx )( this, 'height', px  );
+    }
+
+    // device['width'] = deviceW;
+    // device['height'] = deviceH;
+    // viewport['width'] = viewportW;
+    // viewport['height'] = viewportH;
+    
+    api['viewportW'] = viewportW;
+    api['viewportH'] = viewportH;
+    //api['documentW'] = documentW;
+    //api['documentH'] = documentH;
+    api['deviceW'] = deviceW;
+    api['deviceH'] = deviceH;
+    // api['deviceMax'] = deviceMax;
+    // api['deviceMin'] = deviceMin;
+    api['viewport'] = viewport;
+    api['device'] = device;
+	// api['peek'] = peek;
+    effin['width'] = width;
+    effin['height'] = height;
+    
+    return api;
+
+}));
