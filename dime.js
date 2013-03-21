bfa1cc1a504bf285a3fc3af98fcb3faea27da1b0
Ryan Van Etten <ryanve@gmail.com>
2013-03-21 06:56:04 -0400

    Remove unused definitions. Clean whitespace.

--- dime.js
+++ dime.js
@@ -3,7 +3,7 @@
  * @author    Ryan Van Etten (c) 2012
  * @link      http://github.com/ryanve/dime
  * @license   MIT
- * @version   2.0.0
+ * @version   2.0.1
  */
 
 /*jslint browser: true, devel: true, node: true, passfail: false, bitwise: true, continue: true
@@ -11,11 +11,11 @@
 , regexp: true, undef: true, sloppy: true, stupid: true, sub: true, vars: true, white: true
 , indent: 4, maxerr: 180 */
  
-(function (root, name, definition) {// github.com/umdjs/umd
+(function(root, name, definition) {// github.com/umdjs/umd
     if ( typeof module != 'undefined' && module.exports ) { 
-        module.exports = definition(); // node / common / ender
+        module.exports = definition(); // node|common|ender
     } else { root[name] = definition(); } // browser
-}(this, 'dime', function () {
+}(this, 'dime', function() {
 
     var win = window
       , doc = document
@@ -31,43 +31,36 @@
     * @constructor  a very lo-fi one (it only grabs the 1st elem)
     * @param  {*=}  item
     */
-    function Api (item) {
+    function Api(item) {
         this.length = 0;
-        if ( item = item.nodeType > 0 || item === win ? item : item && item[0] ) {
-            this[ this.length++ ] = item;
+        if (item = item.nodeType > 0 || item === win ? item : item && item[0]) {
+            this[this.length++] = item;
         }
     }
     
     /**
      * @param  {*=}  item
      */
-    function api (item) {
+    function api(item) {
         return new Api(item);
     }
 
     // read @link github.com/ender-js/ender-js/pull/17
     api.prototype = api['fn'] = Api.prototype = effin;
-    //var api = {'fn': effin};
     
     // responsejs.com/labs/dimensions/#device
-    function deviceW () { 
+    function deviceW() { 
         return screenW; 
     }
-    function deviceH () {
+    function deviceH() {
         return screenH; 
-    }
-    function deviceMax () { 
-        return screenMax; 
-    }
-    function deviceMin () { 
-        return screenMin; 
     }
     
     // responsejs.com/labs/dimensions/#viewport
-    function viewportW () {
+    function viewportW() {
         return docElem.clientWidth; 
     }
-    function viewportH () {
+    function viewportH() {
         return docElem.clientHeight; 
     }
 
@@ -75,24 +68,24 @@
      * @param  {string}  name
      * @return {number}
      */    
-    function vpGetter ( name ) {
-        return docElem[ 'client' + capName[name] ];
+    function vpGetter(name) {
+        return docElem['client' + capName[name]];
     }
     
     /**
      * @return {Object}
      */
-    function viewport ( name ) {
+    function viewport(name) {
         return null == name ? {
             'width': docElem.clientWidth
           , 'height': docElem.clientHeight
-        } : vpGetter( name );  
+        } : vpGetter(name);  
     }
 
     /**
      * @return {Object}
      */
-    function device ( name ) {
+    function device(name) {
         var ob = { 'width': screenW, 'height': screenH };
         return null == name ? ob : ob[name];
     }
@@ -101,19 +94,19 @@
      * @param  {string}  name
      * @return {number}
      */
-    function docGetter ( name ) {
+    function docGetter(name) {
         var scroll, offset;
         name = capName[name];
         scroll = 'scroll' + name;
         offset = 'offset' + name;
-        return Math.max( docElem[offset], docElem[scroll], doc.body[scroll] );
+        return Math.max(docElem[offset], docElem[scroll], doc.body[scroll]);
     }
 
     // responsejs.com/labs/dimensions/#document
-    function documentW () {
+    function documentW() {
         return docGetter('width');
     }
-    function documentH () {
+    function documentH() {
         return docGetter('height');
     }
     
@@ -123,16 +116,17 @@
      * @param  {Function}  fn
      * @param  {Object=}   scope
      */
-    function peek ( el, fn, scope ) {
+    function peek(el, fn, scope) {
     
-        if ( !el ) { return; }
+        if (!el) { return; }
         var oPos, oVis, oDis, ret
           , tPos, tVis, tDis
           , w = el.offsetWidth
           , h = el.offsetHeight
           , isHidden = !w && !h;
 
-        if ( isHidden ) {// save orig styles and set temp styles:
+        if (isHidden) {
+            // save orig styles and set temp styles:
             oPos = el.style.position || '';
             oVis = el.style.visibility || '';
             oDis = el.style.display || '';
@@ -141,27 +135,24 @@
             el.style.display = tDis = 'block';
         }
 
-        ret = fn.call( scope || el, el );
+        ret = fn.call(scope || el, el);
         
-        if ( isHidden ) {// put back the orig values, unless `fn` reset them:
+        if (isHidden) {// put back the orig values, unless `fn` reset them:
             tPos === el.style.position && (el.style.position = oPos);
             tVis === el.style.visibility && (el.style.visibility = oVis);
             tDis === el.style.display && (el.style.display = oDis);
         }
-        
         return ret;
-
-    }// peek
+    }
     
     /**
      * @param  {Array|Object}  els
      * @param  {string}        name
      * @param  {string}        value
      */
-    // inlined @ minification
-    function setStyle ( els, name, value ) {
+    function setStyle(els, name, value) {
         var i = 0, l = els.length;
-        while ( i < l ) {
+        while (i < l) {
             els[i++]['style'][name] = value;
         }
         return els;
@@ -173,7 +164,7 @@
      * @param  {string}        name
      * @param  {string|number} px
      */
-    function setPx ( els, name, px ) {
+    function setPx(els, name, px) {
         px = typeof px == 'number' ? '' + px + 'px' : px;
         return setStyle( els, name, px );
     }
@@ -184,17 +175,17 @@
      * @param  {Array|Object}      els
      * @return {number|undefined}
      */
-	function getPx ( els, name ) {
-		if ( els[0] ) {
-            if ( 1 === els[0].nodeType ) {
-                return peek( els[0], function (e) { 
-                    return e[ 'offset' + capName[name] ]; 
+	function getPx(els, name) {
+		if (els[0]) {
+            if (1 === els[0].nodeType) {
+                return peek(els[0], function(e) { 
+                    return e['offset' + capName[name]]; 
                 });
             }
-            if ( els[0] === win ) { 
+            if (els[0] === win) { 
                 return vpGetter(name); 
             }
-            if ( els[0] === doc ) { 
+            if (els[0] === doc) { 
                 return docGetter(name); 
             }
         }
@@ -203,36 +194,22 @@
     /**
      * @param  {(number|string)=}  px
      */
-    function width ( px ) {
-        return ( void 0 === px ? getPx : setPx )( this, 'width', px  );
-    }
+    effin['width'] = function(px) {
+        return (void 0 === px ? getPx : setPx)(this, 'width', px);
+    };
     
     /**
      * @param  {(number|string)=}  px
      */
-    function height ( px ) {
-        return ( void 0 === px ? getPx : setPx )( this, 'height', px  );
-    }
-
-    // device['width'] = deviceW;
-    // device['height'] = deviceH;
-    // viewport['width'] = viewportW;
-    // viewport['height'] = viewportH;
+    effin['height'] = function(px) {
+        return (void 0 === px ? getPx : setPx)(this, 'height', px);
+    };
     
     api['viewportW'] = viewportW;
     api['viewportH'] = viewportH;
-    //api['documentW'] = documentW;
-    //api['documentH'] = documentH;
     api['deviceW'] = deviceW;
     api['deviceH'] = deviceH;
-    // api['deviceMax'] = deviceMax;
-    // api['deviceMin'] = deviceMin;
     api['viewport'] = viewport;
     api['device'] = device;
-	// api['peek'] = peek;
-    effin['width'] = width;
-    effin['height'] = height;
-    
     return api;
-
 }));
